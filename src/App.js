09c01ac779b0832e309c37c0ebb0b6763799226b
ZumDeWald <zumdewald@gmail.com>
2020-03-28 12:01:49 -0500

    Install / test Aphrodite

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={css(styles.body)}>
+      <img src={logo} alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
+
+const styles = StyleSheet.create({
+  body: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#222",
+    color: "#fff",
+    width: '100vw',
+    height: '100vh',
+  }
+})
 
 export default App;
